91d831f13e53ec49c96a3875295481d2c927b89d
Konrad Jagusiak <konrad.jagusiak@gmail.com>
2018-01-13 18:20:51 +0100

    RoleColours

--- Common/DomainEntities/IUserDomainEntity.cs
+++ Common/DomainEntities/IUserDomainEntity.cs
@@ -1,10 +1,14 @@
-﻿namespace Common.DomainEntities
+﻿using System;
+using System.Collections.Generic;
+
+namespace Common.DomainEntities
 {
-    public interface IUserDomainEntity : IDomainEntity
+    public interface IUserDomainEntity : IFormattable, IDomainEntity
     {
         string Name { get; set; }
         string Roles { get; set; }
         string Login { get; set; }
         string Password { get; set; }
+        string GetRoleColor();
     }
 }
--- DomainModel/User.cs
+++ DomainModel/User.cs
@@ -1,14 +1,38 @@
-﻿using Common.DomainEntities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.DomainEntities;
 
 namespace DomainModel
 {
     public class User : IUserDomainEntity
     {
+        readonly Dictionary<string, string> _colours = new Dictionary<string, string>
+        {
+            {"Administrator", "goldenrod"},
+            {"Moderator", "TBD"},
+            {"User", "#93c763"},
+            {"Guest", "initial"}
+        };
+       
         public int ParrentId => Id;
         public string Name { get; set; }
         public string Roles { get; set; }
         public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string GetRoleColor()
+        {
+            var initColor = "initial";
+            var firstRole = Roles.Split(',').Select(x => x.Trim()).FirstOrDefault(role => _colours.TryGetValue(role, out initColor));
+          
+            return initColor;
+        }
+
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return Name;
+        }
     }
 }
